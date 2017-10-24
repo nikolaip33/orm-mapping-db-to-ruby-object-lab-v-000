28b1998288ce9184aa7a10008430e4ef464c9161
Nikolai Pawlowski <nikolaip@gmail.com>
2017-10-24 16:30:35 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -98,4 +98,16 @@ class Student
       self.new_from_db(row)
     end.first
   end
+
+
+  def self.all_students_in_grade_X(x)
+    sql = <<-SQL
+      SELECT * FROM students WHERE grade = ?
+    SQL
+
+
+
+  end
+
+  
 end #class Student
